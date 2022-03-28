ecc7d071a203dd54897768c5087fd64411576be1
Rexaon <93644372+Rexaon@users.noreply.github.com>
2022-03-28 13:46:21 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,17 @@
+function Knap() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+/* */
+
+function bildeBytt() {
+
+  if (document.getElementById("klikkBytte").src == "Bilder/images/try_again.png") 
+  {
+      document.getElementById("klikkBytte").src = "Bilder/images/try_again.png";
+  }
+  else 
+  {
+      document.getElementById("klikkBytte").src = "Bilder/images/try_again.png";
+  }
+}
